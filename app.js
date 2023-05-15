863734837583dbf427accec2c6f62fb7dae9c6cc
Franco Fernández <franfdps4@hotmail.com>
2023-05-15 08:57:35 -0300

    Adding new palette and style, and new page about

---
+++ app.js
@@ -0,0 +1,5 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Update the content of the "currentYear" element with the actual year
+document.getElementById("currentYear").textContent = currentYear;
